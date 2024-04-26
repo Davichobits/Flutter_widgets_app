bea65a3835235271f1c0a05f24c650e9faef39af
Davichobits <davidruizdeveloper@gmail.com>
2024-04-26 05:21:01 -0500

    Home Screen implemented

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 4).getTheme(),
+      theme: AppTheme(selectedColor: 0).getTheme(),
       home: const HomeScreen(),
     );
   }
