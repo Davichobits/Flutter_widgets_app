4b14f5f1efddcac56a20a44fe985215a8eaefc0c
Davichobits <davidruizdeveloper@gmail.com>
2024-04-26 06:03:28 -0500

    Nuttons and cards screens implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,7 +11,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
-      body: _HomeView(),
+      body: const _HomeView(),
     );
   }
 }
@@ -18,8 +19,6 @@ class HomeScreen extends StatelessWidget {
 class _HomeView extends StatelessWidget {
   const _HomeView();
 
-  
-  
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -38,17 +37,24 @@ class _CustomListTile extends StatelessWidget {
     required this.menuItem,
   });
   final MenuItem menuItem;
-// 
+//
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
     return ListTile(
       leading: Icon(menuItem.icon, color: colors.primary),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: colors.primary,
+      ),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
-        //TODO: navegar a otra pantalla
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),
+          )
+        );
       },
     );
   }
